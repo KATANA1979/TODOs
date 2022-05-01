574c29b4f010fe2be963c33a94cc203333288c1d
KATANA1979 <an79is@mail.ru>
2022-05-01 10:19:13 +0300

    diplom-todos

---
+++ src/main/java/ru/netology/javacore/Todos.java
@@ -0,0 +1,21 @@
+package ru.netology.javacore;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Todos {
+    private List<String> tasks = new ArrayList<>();
+
+    public void addTask(String task) {
+        tasks.add(task);
+    }
+
+    public void removeTask(String task) {
+        while (tasks.remove(task)); // Для повторяющихся задач
+    }
+
+    public String getAllTasks() {
+        return tasks.stream().sorted().collect(Collectors.joining(" "));
+    }
+
+}
